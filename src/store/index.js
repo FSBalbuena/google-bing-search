7217fb8766ec8284b2bc0907f3f87bfa615e8e22
FSBalbuena <fsbalbuena@gmail.com>
2024-10-20 14:20:45 -0300

    Fix reactDOM.render vs createRoot

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import allReducers from './reducers';
 
 const allMiddlewares = [thunk];
